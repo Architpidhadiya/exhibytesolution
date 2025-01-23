2437eefc544c482b3bac6302d88a5705c7f2b297
Architpidhdiya <archit.pidhadiya@exhibytesolution.com>
2025-01-23 14:45:09 +0530

    Dynamic Imports

--- first.js
+++ first.js
@@ -1,7 +1,19 @@
 //import { name, message, Reply } from "./second.js";
-import hello, * as m from "./second.js"
+//import hello, * as m from "./second.js"
 
-console.log(m.name)
+/*console.log(m.name)
 m.message()
 hello()
-let r = new m.Reply()
+let r = new m.Reply()*/
+
+//import {add} from "./second.js"
+
+const loadModule = async () => {
+    try {
+        const module = await import('./second.js')
+        console.log(module.add(2, 3))
+    } catch (error) {
+        console.error("Error", error)
+    }
+};
+loadModule()
