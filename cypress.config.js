c02002067bbe9edb6f011c1555aff7fb5869ab93
Architpidhdiya <archit.pidhadiya@exhibytesolution.com>
2025-02-19 14:32:32 +0530

    Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
-module.exports = {
-  video: true,
-  screenshotOnRunFailure: true,
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
-    baseUrl: 'http://example.com',
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
-}
+});
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,47 +1,53 @@
-/*describe('My First Cypress Test', () => {
-    it('Should visit a page and check an element', () => {
-      cy.visit('https://youtube.com');
-      cy.contains('Example Domain').should('be.visible');
-    });
-  });*/
+describe("First Test", () => {
 
-  /*describe('Basic Cypress Test', () => {
-    it('Should visit a page and check for an element', () => {
-  
-      cy.visit('https://youtube.com');
-  
-      cy.get('h1').should('be.visible');
-  
-      cy.contains('About').click();
-  
-      cy.url().should('include', '/about');
-    });
-  });*/
+    beforeEach(() => {
+        //cy.visit("about:blank")
+        cy.document().then((doc) => {
+        doc.body.innerHTML = `
 
-  /*describe('Element Visibility Test', () => {
-    it('Should show and hide an element when a button is clicked', () => {
-      cy.visit('https://example.com');
-  
-      cy.get('.hidden-element').should('not.be.visible');
+                <form id="loginForm">
+                    <label for="username">Username:</label>
+                    <input type="test" id="username" name="username" placeholder="Enter Your Name" required/>
+                    
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" placeholder="Enter Your Password" required/>
 
-      cy.get('.show-button').click();
-  
-      cy.get('.hidden-element').should('be.visible');
-    });
-  });*/  
+                    <button type="button" id="loginBtn">Login</button>
+                </form>
 
-  
-  describe('Login Test', () => {
-    it('Should log in successfully with valid credentials', () => {
-      cy.visit('/login');
-  
-      cy.get('input[name="username"]').type('user123');
-      cy.get('input[name="password"]').type('password123');
-  
-      cy.get('button[type="submit"]').click();
-  
-      cy.url().should('include', '/dashboard');
-      cy.contains('Welcome, user123').should('be.visible');
-    });
-  });
-  
+                <p id="message"></p>    
+            `
+
+                    const loginLogic = () => {
+                        let username = doc.getElementById("username").value
+                        let password = doc.getElementById("password").value
+                        let message = doc.getElementById("message")
+
+                        if (username === "admin" && password === "admin123") {
+                            message.textContent = "Login Successful"
+                            message.style.color = "green"
+                        } else {
+                            message.textContent = "Invalid Username and Password"
+                            message.style.color = "red"
+                        }
+                    }
+                doc.getElementById("loginBtn").addEventListener("click", loginLogic)
+        })
+    })
+
+    it("Login Successful", () => {
+        cy.get("#username").type("admin")
+        cy.get("#password").type("admin123")
+        cy.get("#loginBtn").click()
+        //cy.contains("Login Successful")
+        cy.get("#message").should("have.text", "Login Successful")      
+    })
+
+    it("Error", () => {
+        cy.get("#username").type("archit")
+        cy.get("#password").type("admin1234")
+        cy.get("#loginBtn").click()
+        //cy.contains("Invalid Username and Password")
+        cy.get("#message").should("have.text", "Invalid Username and Password")
+    })
+})
