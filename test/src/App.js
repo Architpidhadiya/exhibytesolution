1d68bdc860781f934b241c6b9f58552db00322c4
Architpidhdiya <archit.pidhadiya@exhibytesolution.com>
2025-02-20 15:17:11 +0530

    react

---
+++ test/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React from "react"
+import Greeting from './greeting';
+
+const App = () => {
+  return (
+    <div>
+      <Greeting name="Archit" />
+      </div>
+  )
+}
+
+export default App;
