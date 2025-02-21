94e0b51c5efc692214b1c5c20d4ba1c38ef7f77d
Architpidhadiya <archit.pidhadiya@exhibytesolution.com>
2025-02-21 13:01:38 +0530

    vue

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+nav {
+  padding: 10px;
+}
+nav a {
+  margin-right: 10px;
+  text-decoration: none;
+  color: blue;
+}
+</style>
---
+++ my-vue-app/src/main.js
@@ -0,0 +1,25 @@
+// import { createApp } from 'vue'
+
+
+// createApp(App).mount('#app')
+
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from './App.vue'
+
+const Home = { template: "<h1>Home Page</h1>" };
+const About = { template: "<h1>About Page</h1>" };
+
+const routes = [
+    { path: "/", component: Home },
+    { path: "/about", component: About }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
