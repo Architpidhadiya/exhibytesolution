b8c92e74d1ffc142fee3edd99f8f7964e051b22a
Architpidhadiya <archit.pidhadiya@exhibytesolution.com>
2025-02-21 18:34:33 +0530

    Calculator

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,9 +1,41 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view></router-view>
-    </div>
+  <div id="app">
+    <h2>To-Do List</h2>
+    <input v-model="newTask" placeholder="Enter a task">
+    <button @click="addTask">Add</button>
+
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }} <button @click="removeTask(index)"> X </button>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim()) {
+        this.tasks.push(this.newTask);
+        this.newTask = '';
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  margin-left: 10px;
+}
+</style>
