db42b61b0269b84746d518ba517bf14a961bf4e3
Architpidhdiya <archit.pidhadiya@exhibytesolution.com>
2025-02-24 15:46:26 +0530

    Counter

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,27 +1,14 @@
-// import { createApp } from 'vue'
+import { createApp, h, ref } from "vue";
 
+const App = {
+    setup() {
+        const count = ref(0)
+        return () =>
+            h("div", [
+                h("p", `Count: ${count.value}`),
+                h("button", { onClick: () => count.value++ }, "Increment")
+            ])
+    }
+}
 
-// createApp(App).mount('#app')
-
-import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
-import App from './App.vue'
-import Home from "./components/Home.vue";
-import About from "./components/About.vue";
-
-//const Home = { template: "<h1>Home Page</h1>" };
-//const About = { template: "<h1>About Page</h1>" };
-
-const routes = [
-    { path: "/", component: Home },
-    { path: "/about", component: About }
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
-
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
+createApp(App).mount("#app")
